8031dcce154a6995761b833a33d77912dee84c11
John Remon <john1812008@gmail.com>
2026-05-10 23:06:22 +0200

    added the instruction set as enum

---
+++ src/main/java/com/interpreter/Instruction.java
@@ -0,0 +1,23 @@
+package com.interpreter;
+
+public enum Instruction {
+    MOV,
+    ADD,
+    SUB,
+    MUL,
+    DIV,
+    AND,
+    OR,
+    XOR,
+    NOT,
+    SHL,
+    LOAD,
+    STORE,
+    CMP,
+    JMP,
+    JZ,
+    JN,
+    JNZ,
+    PRINT,
+    HALT
+}
