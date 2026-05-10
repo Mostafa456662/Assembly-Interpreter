350f557e33b49bb7417cbdc6a0bae2cd6ad55328
John Remon <john1812008@gmail.com>
2026-05-10 23:17:13 +0200

    initialized the basic expression class

--- src/main/java/com/interpreter/model/Expression.java
+++ src/main/java/com/interpreter/model/Expression.java
@@ -1,5 +1,21 @@
-package com.interpreter;
+package com.interpreter.model;
+
+import java.util.List;
 
 public class Expression {
+    private Instruction instruction;
+    private List<String> arguments;
 
+    public Expression(Instruction instruction, List<String> arguments) {
+        this.instruction = instruction;
+        this.arguments = arguments;
+    }
+
+    public Instruction getInstruction() {
+        return instruction;
+    }
+
+    public List<String> getArguments() {
+        return arguments;
+    }
 }
