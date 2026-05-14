2f5a2e8fa89ba84699e9bcc910c4756760bbc838
Mostafa <fastsafa.2006@gmail.com>
2026-05-14 13:16:19 +0200

    feat: execution and branching functions

--- src/main/java/com/interpreter/engine/Executor.java
+++ src/main/java/com/interpreter/engine/Executor.java
@@ -1,106 +1,190 @@
 package com.interpreter.engine;
 
 import com.interpreter.model.Expression;
+import com.interpreter.model.Instruction;
+
 import java.util.HashMap;
+import java.util.List;
 
 public class Executor {
     private int[] memory;
     private int[] registers;
     private boolean zeroFlag;
     private boolean negativeFlag;
+    private int pc;
 
-    private HashMap<Integer, Expression> instructions;
+    private HashMap<Integer, Expression> expressions;
 
-    public Executor(int[] memory, int[] registers, HashMap<Integer, Expression> instructions) {
+    public Executor(int[] memory, int[] registers, HashMap<Integer, Expression> expressions) {
         this.memory = memory;
         this.registers = registers;
         this.zeroFlag = false;
         this.negativeFlag = false;
-        this.instructions = instructions;
+        this.expressions = expressions;
+        this.pc = 0;
     }
 
     // This is the one that calls the needed method
     public void execute() {
+        Expression currentExpression = expressions.get(pc);
+        Instruction currentInstruction = currentExpression.getInstruction();
+        List<String> arguments = currentExpression.getArguments();
+        switch (currentInstruction) {
+            case MOV:
+                mov(arguments);
+                break;
+            case ADD:
+                add(arguments);
+                break;
+            case SUB:
+                sub(arguments);
+                break;
+            case MUL:
+                mul(arguments);
+                break;
+            case DIV:
+                div(arguments);
+                break;
+            case AND:
+                and(arguments);
+                break;
+            case OR:
+                or(arguments);
+                break;
+            case XOR:
+                xor(arguments);
+                break;
+            case NOT:
+                not(arguments);
+                break;
+            case SHL:
+                shl(arguments);
+                break;
+            case SHR:
+                shr(arguments);
+                break;
+            case LOAD:
+                load(arguments);
+                break;
+            case STORE:
+                store(arguments);
+                break;
+            case CMP:
+                cmp(arguments);
+                break;
+            case JMP:
+                jmp(arguments);
+                break;
+            case JZ:
+                jz(arguments);
+                break;
+            case JN:
+                jn(arguments);
+                break;
+            case JNZ:
+                jnz(arguments);
+                break;
+            case PRINT:
+                print(arguments);
+                break;
+            case HALT:
+                halt();
+                break;
+            default:
+                throw new IllegalArgumentException("Unknown instruction: " + currentInstruction);
+        }
+    }
+
+    private void mov(List<String> arguments) {
 
     }
 
-    private void mov() {
+    private void add(List<String> arguments) {
 
     }
 
-    private void add() {
+    private void sub(List<String> arguments) {
 
     }
 
-    private void sub() {
+    private void mul(List<String> arguments) {
 
     }
 
-    private void mul() {
+    private void div(List<String> arguments) {
 
     }
 
-    private void div() {
+    private void and(List<String> arguments) {
 
     }
 
-    private void and() {
+    private void or(List<String> arguments) {
 
     }
 
-    private void or() {
+    private void xor(List<String> arguments) {
 
     }
 
-    private void xor() {
+    private void not(List<String> arguments) {
 
     }
 
-    private void not() {
+    private void shl(List<String> arguments) {
 
     }
 
-    private void shl() {
+    private void shr(List<String> arguments) {
 
     }
 
-    private void shr() {
+    private void load(List<String> arguments) {
 
     }
 
-    private void load() {
+    private void store(List<String> arguments) {
 
     }
 
-    private void store() {
+    private void cmp(List<String> arguments) {
 
     }
 
-    private void cmp() {
-
+    private void jmp(List<String> arguments) {
+        int targetAddress = Integer.parseInt(arguments.get(0));
+        pc = targetAddress;
     }
 
-    private void jmp() {
-
+    private void jz(List<String> arguments) {
+        if (zeroFlag) {
+            int targetAddress = Integer.parseInt(arguments.get(0));
+            pc = targetAddress;
+        }
     }
 
-    private void jz() {
-
+    private void jn(List<String> arguments) {
+        if (negativeFlag) {
+            int targetAddress = Integer.parseInt(arguments.get(0));
+            pc = targetAddress;
+        }
     }
 
-    private void jn() {
-
+    private void jnz(List<String> arguments) {
+        if (!zeroFlag) {
+            int targetAddress = Integer.parseInt(arguments.get(0));
+            pc = targetAddress;
+        }
     }
 
-    private void jnz() {
-
-    }
-
-    private void print() {
+    private void print(List<String> arguments) {
+        String register = arguments.get(0);
+        int registerIndex = Integer.parseInt(register.substring(1));
+        System.out.println(registers[registerIndex]);
 
     }
 
     private void halt() {
-
+        System.exit(0);
     }
 }
