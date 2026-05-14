9bb1966e940367c7489c18f8c057b1086b20bc12
Moaz Elborollosy <mozaelborollosy@gmail.com>
2026-05-14 13:42:30 +0200

    Parser fix

--- src/main/java/com/interpreter/engine/Parser.java
+++ src/main/java/com/interpreter/engine/Parser.java
@@ -8,11 +8,12 @@ import com.interpreter.model.Instruction;
 
 public class Parser {
 
-    public static HashMap<String, Expression> resolveLabels(List<Expression> expressions) {
-        HashMap<String, Expression> labels = new HashMap<>();
-        for (Expression exp : expressions) {
+    public static HashMap<String, Integer> resolveLabels(List<Expression> expressions) {
+        HashMap<String, Integer> labels = new HashMap<>();
+        for (int i = 0; i < expressions.size(); i++) {
+            Expression exp = expressions.get(i);
             if (exp.getLabel() != null) {
-                labels.put(exp.getLabel(), exp);
+                labels.put(exp.getLabel(), i);
             }
         }
         return labels;
@@ -20,7 +21,7 @@ public class Parser {
 
     public static HashMap<Integer, Expression> assignPC(List<Expression> expressions) {
 
-        HashMap<String, Expression> labelMap = resolveLabels(expressions);
+        HashMap<String, Integer> labelMap = resolveLabels(expressions);
         HashMap<Integer, Expression> pc = new HashMap<>();
 
         for (int i = 0; i < expressions.size(); i++) {
@@ -28,10 +29,8 @@ public class Parser {
             if (inst == Instruction.JMP || inst == Instruction.JNZ || inst == Instruction.JZ
                     || inst == Instruction.JN) {
                 String label = expressions.get(i).getArguments().get(0);
-                Expression targetExp = labelMap.get(label);
-                int targetPC = expressions.indexOf(targetExp);
-                List<String> newArgs = new ArrayList<>();
-                newArgs.add(String.valueOf(targetPC));
+                int targetPC = labelMap.get(label);
+                List<String> newArgs = new ArrayList<>(List.of(String.valueOf(targetPC)));
                 Expression newExp = new Expression(expressions.get(i).getLabel(), inst, newArgs);
                 pc.put(i, newExp);
             }
